b51a86465e096af2829a89113cfa68719253608c
Sravyanth01 <118617960+Sravyanth01@users.noreply.github.com>
2023-04-23 13:00:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,56 +1,31 @@
-import pandas as pd
+
 import pickle
 import streamlit as st
+import numpy as np
 
 # Load the trained model and feature scaler
-model = pickle.load(open('C:/Users/hello/OneDrive/Desktop/ML Project/trained_srav.sav','rb'))
+model = pickle.load(open('C:/Users/91831/Downloads/Melbourne_House_Price1-main/trained_srav.sav','rb'))
 
-# Define a function to preprocess the input data
+st.title('Melbourne Housing Price Predictor')
 
+rooms = st.slider('Rooms', 1, 10, 3)
+distance = st.slider('Distance', 0, 50, 10)
+bathroom = st.slider('Bathroom', 1, 5, 2)
+Propertycount = st.slider('Propertycount:', 0, 1000, 100)
+landsize = st.slider('Landsize', 0, 1000, 500)
+building_area = st.slider('Building Area', 0, 1000, 500)
+year_built = st.slider('Year Built', 1800, 2022, 2000)
 
-# Define the Streamlit app
-def app():
-    # Set the app title
-    st.title('Melbourne Housing Price Predictor')
+def predict():
+    float_features = [float(x) for x in [rooms,distance,bathroom,Propertycount,landsize,building_area,year_built]]
+    final_features = [np.array(float_features)]
+    prediction = model.predict(final_features)
+    label = prediction[0]
     
-    # Define the input form
-    st.sidebar.title('Input Features')
-    Regionname = st.text_input('Region Name:')
-    rooms = st.sidebar.slider('Rooms', 1, 10, 3)
-    distance = st.sidebar.slider('Distance', 0, 50, 10)
-    bathroom = st.sidebar.slider('Bathroom', 1, 5, 2)
-    car = st.sidebar.slider('Car', 0, 4, 1)
-    Type = st.selectbox('Type:',('H','t','u'))
-    SellerG = st.selectbox('SellerG',('Agency','Agent'))
-    Propertycount = st.sidebar.slider('Propertycount:', 0, 1000, 100)
-    landsize = st.sidebar.slider('Landsize', 0, 1000, 500)
-    building_area = st.sidebar.slider('Building Area', 0, 1000, 500)
-    year_built = st.sidebar.slider('Year Built', 1800, 2022, 2000)
-    
-    # Create a pandas DataFrame with the input data
-    input_data = pd.DataFrame({
-        'Rooms': [rooms],
-        'Distance': [distance],
-        'Bathroom': [bathroom],
-        'Car': [car],
-        'Landsize': [landsize],
-        'BuildingArea': [building_area],
-        'YearBuilt': [year_built],
-        'Regionname':[Regionname],
-        'Type':[Type],
-        'SellerG':[SellerG],
-        'Propertycount':[Propertycount]
-       
-    })
-    
-    # Preprocess the input data
-    
-    # Make predictions using the pre-trained model
-    predictions = model.predict(input_data)[0]
-    
-    # Display the predicted price
-    st.subheader('Predicted Price')
-    st.write('${:,.2f}'.format(predictions))
+    print(type(label))
+    print(label)
 
-if __name__ == '__main__':
-    app()
+    st.success('Recommended crop to grow is : ' + str(label) + ' :thumbsup:')
+    
+trigger = st.button('Predict', on_click=predict)
+
